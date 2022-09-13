cd183f296586c623c65c5ee9d199ec735c8579cd
Sam Cain <47900036+samcain1996@users.noreply.github.com>
2022-09-13 16:02:51 -0500

    Initial commit

---
+++ FixAndLaunch.py
@@ -0,0 +1,69 @@
+"""
+
+For some stupid reason, the Epic Games version of Hitman III
+only launches when PipelineCache.bin and PipelineLibrary.bin
+are deleted from the AppData folder. 
+
+This script deletes those files, then launches the game.
+
+"""
+
+
+import os
+import sys
+
+
+# Extract hitman directories 
+def getDirsFromConfigFile():
+    config_file = open(config_filepath, 'r')
+    config_vars = [ var[var.find('=') + 1:].replace('\n', ' ').strip() for var in config_file.readlines() ]
+
+    if len(config_vars) != 2:
+        print('Unexpected number of configuration variables')
+        exit()
+
+    return config_vars
+
+# Exit script if directories are not set
+def abortIfNoDirs(message=None):
+    if len(hitman_stupid_files_dir) == 0 or len(hitman_launcher_dir) == 0:
+        print(message)
+        exit()
+
+
+config_filepath = os.curdir + '\\' + "HitmanIIIFix.conf"
+
+hitman_stupid_files_dir = str()
+hitman_launcher_dir     = str()
+
+# Did the user supply the folders as arguments?
+user_supplied_folders = len(sys.argv) == 3
+
+if user_supplied_folders:
+    hitman_stupid_files_dir = sys.argv[1]
+    hitman_launcher_dir = sys.argv[2]
+
+# Write to or read from config file
+if not os.path.exists(config_filepath) or user_supplied_folders:
+
+    # Create config file
+    config_file = open(config_filepath, 'w')
+    config_file.write('hitman_stupid_files_dir={0}\nhitman_launcher_dir={1}'.format(hitman_stupid_files_dir, hitman_launcher_dir))
+    config_file.close()
+
+    abortIfNoDirs('Folder paths not set in config file, or supplied as arguments')
+
+else:
+    hitman_stupid_files_dir, hitman_launcher_dir = getDirsFromConfigFile()
+
+    abortIfNoDirs('Folder paths not set! Aborting...')
+
+
+# Remove files to allow game to launch
+for file in os.listdir(hitman_stupid_files_dir):
+    os.remove(hitman_stupid_files_dir + '\\' + file)
+
+
+# Launch game
+game_dir = '\"' + hitman_launcher_dir + '\\' + 'Launcher.exe' + '\"'
+os.system(game_dir)
